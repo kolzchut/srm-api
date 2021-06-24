2b1520dc7b057945db0d77f3dbc32ad328f19643
Adam Kariv <adam.kariv@gmail.com>
2021-06-24 23:38:55 +0300

    server side implementation

---
+++ server.py
@@ -0,0 +1,46 @@
+import os
+import logging
+
+from flask import Flask
+from flask_cors import CORS
+
+import elasticsearch
+
+from apisql import apisql_blueprint
+from apies import apies_blueprint
+
+app = Flask(__name__)
+CORS(app, supports_credentials=True)
+
+# SQL API
+app.register_blueprint(
+    apisql_blueprint(
+        connection_string=os.environ['DATABASE_READONLY_URL'],
+        max_rows=10000, debug=False
+    ),
+    url_prefix='/api/db/'
+)
+
+# ES API
+index_name = os.environ['ES_INDEX_NAME']
+blueprint = apies_blueprint(app,
+    [
+        os.environ['ES_DATAPACKAGE']
+    ],
+    elasticsearch.Elasticsearch([dict(host=os.environ['ES_HOST'], port=int(os.environ['ES_PORT']))], timeout=60),
+    dict(
+        point=index_name
+    ),
+    f'{index_name}__docs',
+)
+app.register_blueprint(blueprint, url_prefix='/api/idx/')
+
+
+
+if __name__=='__main__':
+    app.run()
+else:
+    gunicorn_error_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers.extend(gunicorn_error_logger.handlers)
+    app.logger.setLevel(logging.DEBUG)
+    app.logger.info('SERVER STARTING')
