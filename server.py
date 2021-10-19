fac26a55e1e028b8d7442e39e29e720cd2c5cead
Adam Kariv <adam.kariv@gmail.com>
2021-10-19 15:58:32 +0300

    Add responses datatype

--- server.py
+++ server.py
@@ -23,7 +23,7 @@ app.register_blueprint(
 
 # ES API
 index_name = os.environ['ES_INDEX_NAME']
-TYPES = ['cards', 'places']
+TYPES = ['cards', 'places', 'responses']
 datapackages = [x.strip() for x in os.environ['ES_DATAPACKAGE'].split('\n') if x.strip()]
 blueprint = apies_blueprint(app,
     datapackages,
