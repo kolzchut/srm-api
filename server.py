e891dcf1fe208cf977d5b3bc51a6ab18894d20f8
Adam Kariv <adam.kariv@gmail.com>
2022-01-12 16:35:14 +0200

    Add geo_data index

--- server.py
+++ server.py
@@ -98,7 +98,7 @@ app.register_blueprint(
 
 # ES API
 index_name = os.environ['ES_INDEX_NAME']
-TYPES = ['cards', 'places', 'responses', 'points', 'presets']
+TYPES = ['cards', 'places', 'responses', 'points', 'presets', 'geo_data']
 datapackages = [x.strip() for x in os.environ['ES_DATAPACKAGE'].split('\n') if x.strip()]
 blueprint = apies_blueprint(app,
     datapackages,
