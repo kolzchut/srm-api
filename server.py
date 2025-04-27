ee321ea1b0f7142d6b5a3fa4f165c08db3af717c
Amir <amirb4@gmail.com>
2025-04-27 15:52:36 +0300

    sort national_service work, trying to add address_parts.primary

--- server.py
+++ server.py
@@ -160,6 +160,7 @@ class SRMQuery(Query):
                                 'size': 1000,
                                 'sort': [
                                     {'national_service': {'order': 'desc'}},
+                                    {'address_parts.primary': {'order': 'asc', 'missing': '_last'}},
                                 ],
                                 '_source': [
                                     'card_id',
