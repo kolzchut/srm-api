161e68a662d8433b74039f846b2e877505a8a381
arielrepos <ariel_ohana@webiks.com>
2025-05-15 10:40:19 +0300

    Test

--- server.py
+++ server.py
@@ -156,10 +156,10 @@ class SRMQuery(Query):
                             'inner_hits': {
                                 'name': 'collapse_hits',
                                 'size': 1000,
-                                'sort': [
-                                    {'national_service': {'order': 'desc'}},
-                                    # {'address_parts.primary.keyword': {'order': 'asc', 'missing': '_last'}},
-                                ],
+                                # 'sort': [ ## Ariel Test disabling this
+                                #     # {'national_service': {'order': 'desc'}},
+                                #     # {'address_parts.primary.keyword': {'order': 'asc', 'missing': '_last'}},
+                                # ],
                                 '_source': [
                                     'card_id',
                                     'organization_name',
